7c15ac85a795caf02a5eb874de18edbea0d3c88c
Benjamin Hasker <benjaminhasker@benjamins-mbp.attlocal.net>
2018-12-11 21:28:46 -0600

    now working with terminal args

--- glitch_random.py
+++ glitch_random.py
@@ -1,4 +1,5 @@
-#glitcher
+#!/usr/bin/env python2.7
+
 
 import random, sys
 
@@ -10,28 +11,25 @@ else:
 	print("\nUsage:\n     glitch_random.py inputfile outputfile\n")
 	exit()
 
-print("inputfile: ", str(sys.argv[1]))
-print("outputfile: ", str(sys.argv[2]))
+print "inputfile: ", str(sys.argv[1])
+print "outputfile: ", str(sys.argv[2]) 
 
 
 file = open(inputfile,"r") #mess_with.txt is the filename to read
-								#it'd be great if this could be done via the commandline, without converting to .txt
-print("read file")
 header = []
-for num in range(0,200): #read past the header lines, stored for later
-	header.append(file.readline(num))
-
 
 body = []
+							#it'd be great if this could be done via the commandline, without converting to .txt
+for line in file:
+	body.append(line)
+
+for num in range(201):
+	header.append(body.pop(0))
+
 
 #loads the whole file into body
 #shouldn't we go line by line? Things are very inneficient here
 #the advantage of loading the whole file in, is that glitch can run multiple times on one file, increasing the variety of effects
-for line in file:  
-	body.append(line)
-
-for num in range(201): #further removes some part from the body (the ending of the file shouldn't be messed with either)
-	body.pop(0)
 
 file.close()
 
@@ -84,15 +82,15 @@ def glitch(body,interval,offset):
 
 
 #these settings have gotten good results
-body = glitch(body,2000,4)
-body = glitch(body,200,50)
+#body = glitch(body,2000,4)
+#body = glitch(body,200,50)
 #body = glitch(body,25,13)
 #body = glitch(body,23,49)
 
 
 
-for i in range(2): #an easy way to reglitch multiple times 
-	body = glitch(body,1049-i,1)
+for i in range(10): #an easy way to reglitch multiple times 
+	body = glitch(body,3500-i,2000)
 
 newfile.writelines(header)
 
