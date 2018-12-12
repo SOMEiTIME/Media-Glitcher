ea7a71a0a24db7181de6118dd692ef74327cf631
Benjamin Hasker <benjaminhasker@benjamins-mbp.attlocal.net>
2018-12-11 20:44:27 -0600

    tried and failed at making this work bytewise

--- glitch_random.py
+++ glitch_random.py
@@ -1,6 +1,6 @@
 #glitcher
 
-import random, sys
+import random, sys, os
 
 
 if len(sys.argv) >= 2:
@@ -10,35 +10,30 @@ else:
 	print("\nUsage:\n     glitch_random.py inputfile outputfile\n")
 	exit()
 
-print("inputfile: ", str(sys.argv[1]))
-print("outputfile: ", str(sys.argv[2]))
 
 
-file = open(inputfile,"r") #mess_with.txt is the filename to read
+file = open(inputfile,"rb") #mess_with.txt is the filename to read
 								#it'd be great if this could be done via the commandline, without converting to .txt
-print("read file")
+
 header = []
 for num in range(0,200): #read past the header lines, stored for later
 	header.append(file.readline(num))
 
 
-body = []
 
 #loads the whole file into body
 #shouldn't we go line by line? Things are very inneficient here
 #the advantage of loading the whole file in, is that glitch can run multiple times on one file, increasing the variety of effects
-for line in file:  
-	body.append(line)
-
-for num in range(201): #further removes some part from the body (the ending of the file shouldn't be messed with either)
-	body.pop(0)
+body = file.read(os.stat(sys.argv[1]).st_size-20); #how much to not mess with?
+#for num in range(201): #further removes some part from the body (the ending of the file shouldn't be messed with either)
+	#body.pop(0)
 
 file.close()
 
-newfile = open(outputfile,"w") #the new, mangled version of the file will be called new.txt
+newfile = open(outputfile,"wb") #the new, mangled version of the file will be called new.txt
 							#this could also be done through the commandline
 
-
+#need to change these string based operations into byte based operations
 #this function is the heart of the program, glitch will cause video distortion effects of length interval
 	#it does this via swapping data for a certain length
 def glitch(body,interval,offset): 
@@ -48,14 +43,16 @@ def glitch(body,interval,offset):
 
 		if counter == interval:
 			place1 = num
-			place2 = num-(1+random.randint(0,2)) #introduced randomness here for more organic looking results
+			place2 = num-(1+random.randint(0,20)) #introduced randomness here for more organic looking results
+			
+
 			len1 = len(body[place1])
 			len2 = len(body[place2])
 			original = body[place1]
 			newstring = ""
 			if len1 < len2:
 				for char in body[place1]:
-					newstring = newstring + char
+					newstring = newstring + char 
 				count = 0
 				for char in body[place2]:
 					if count >= len(body[place1])-4:
@@ -86,7 +83,7 @@ def glitch(body,interval,offset):
 #these settings have gotten good results
 body = glitch(body,2000,4)
 body = glitch(body,200,50)
-#body = glitch(body,25,13)
+body = glitch(body,25,13)
 #body = glitch(body,23,49)
 
 
@@ -96,7 +93,7 @@ for i in range(2): #an easy way to reglitch multiple times
 
 newfile.writelines(header)
 
-newfile.writelines(body)
+newfile.write(body)
 
 
 
