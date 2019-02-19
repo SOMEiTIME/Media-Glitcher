26ff5f8b1554ccab02fc3d904c97885de28ab522
Benjamin Hasker <benjaminhasker@Benjamins-MacBook-Pro.local>
2019-02-18 20:40:59 -0600

    corrupt_glitcher is itself again

--- corrupt_glitcher.py
+++ corrupt_glitcher.py
@@ -6,29 +6,34 @@ import random, sys, os
 class corrupt_glitcher(glitcher):
 
     def glitch(self,body,interval,flavour):     
+        corruption = 1
         counter = 0
-        offset_counter = 0
         for num in range(0,len(body)):
 
             if counter == interval:
-                place1 = num
-                place2 = num-(1+random.randint(0,2 + flavour % 10)) #introduced randomness here for more organic looking results
-                
 
-                len1 = len(body[place1])
-                len2 = len(body[place2])
-                original = body[place1]
+
+                for i in range(corruption):
+
+                    place1 = num-i
+                    place2 = num-(i+1+random.randint(0,5+corruption))
+                    len1 = len(body[place1])
+                    len2 = len(body[place2])
+                    original = body[place1]
                 if len1 < len2:
-                    new_line = super(corrupt_glitcher,self)._generate_fitting_line(body,place1,place2);
+                    new_line =  super(corrupt_glitcher,self)._generate_fitting_line(body,place1,place2);
                 else:
-                    new_line = super(corrupt_glitcher,self)._generate_fitting_line(body,place2,place1);
+                    new_line =  super(corrupt_glitcher,self)._generate_fitting_line(body,place2,place1);
+                    
 
-                body[place1] = new_line[:len(new_line)-4]
+                    body[place1] = new_line[:len(new_line)-4]
 
-                if len(body[place1]) != len(original):
-                    body[place1] = original
+                    if len(body[place1]) != len(original):
+                        body[place1] = original
 
-                counter = flavour
+                counter = 0
+                corruption = corruption + 1
             else:
-                counter = counter + 1 
+                counter = counter + 1
+
         return body 
